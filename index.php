b49a4e70f3fc787253181eaec4cd8732805ddf8d
MrRembes <RembesRemb@gmail.com>
2023-10-20 22:47:44 +0200

    Pobrane od Nergala

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>index</title>
+    <meta name="description" content="opis" />
+    <meta name="keywords" content="slowa klucz" />
+    <meta http-equiv="X-YA-Compatible" content="IE=edge,chrome=1" />
+
+    <link rel="stylesheet" href="index.css"> <!-- CSS -->
+
+</head>
+
+<body>
+
+    <?php
+    
+    // losowanie \\
+    session_start();
+
+    $db = new mysqli('localhost', 'root', '', 'angielski_slowka'); // Łączenie z bazą danych \\
+    $table = "ang_19_09_2023";
+
+    $q = "SELECT count(*) FROM $table"; // Tabela z której bierze dane \\
+    $result = $db->query($q);
+    $row = $result->fetch_row();
+    $count = $row[0];
+
+    $id_pytanie = array(
+            rand(1, $count) // Losowanie słówka \\
+    );
+
+    $ilosc_pytan = 15;
+
+    // Losowanie i dopisanie do arraya liczb bez powtórzeń \\
+    for($i = 0; $i < $ilosc_pytan-1; $i++) { 
+
+        $rand = rand(1, $count);
+        $break = false;
+
+        for($j = 0; $j <= $i; $j++) {
+            if($id_pytanie[$j] == $rand) {
+            $i--;
+            $break = true;
+            }
+        }
+        if(!$break) {
+            array_push ($id_pytanie, $rand);
+        }
+        
+    }
+    sort($id_pytanie); // Sprawdzenie czy są powtórzenia \\
+
+    $slowo = array();
+    $popr_odp = array();
+    $popr_odp_2 = array();
+
+    //1
+    $ids = join("','", $id_pytanie); // join(separator, array) \\
+    $q = "SELECT * FROM $table WHERE id IN ('$ids')"; // Tabela z której bierze dane \\
+    $result = $db->query($q);
+
+    while ($row = $result->fetch_array()) {
+        array_push ($slowo, $row[1]);
+        array_push ($popr_odp, $row[2]);
+        array_push ($popr_odp_2, $row[3]);
+    }
+
+    for($i=0; $i < $ilosc_pytan; $i++) {
+        $popr_odp[$i] = strtolower($popr_odp[$i]);
+        $popr_odp_2[$i] = strtolower($popr_odp_2[$i]);
+    }
+
+    ?>
+
+    <?php 
+    //formulash \\
+    $_SESSION['slowo'] = $slowo;
+    $_SESSION['popr_odp'] = $popr_odp;
+    $_SESSION['popr_odp2'] = $popr_odp_2;
+    $_SESSION['ilosc_pytan'] = $ilosc_pytan;
+    echo "<center><div>";
+    echo '<form action="wyniki.php" method="post">';
+    echo "<h2>Przetłumacz na polski: </h2>";
+    
+    for ($i = 0; $i < $ilosc_pytan; $i++) {
+    
+        echo $i+1;
+        echo '. ';
+
+        print_r ($slowo[$i]); 
+        echo "<br />";
+
+        echo "<input type='text'"; 
+        echo "name='odpowiedz$i'"; // Numer słówka
+        echo "required autocomplete='off'/><br />"; // autocomplete='off' bez odpowiedzi
+    }
+    echo "<br /><input type='submit' value='Wyślij' />";
+    echo "</form></div></center>";
+
+// Wybór tabeli na początku \\
+// Wybór czy z ang-pl czy pl-ang \\
+// Zrobić CSS \\
+// Dodać przycisk podpowiedzi pokazujący pierwszą litere \\
+// github \\
+
+// Code by Najdz3l & MrRembes \\
+    ?>
+</body>
+
+</html>
---
+++ wyniki.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>wyniki</title>
+    <meta name="description" content="opis" />
+    <meta name="keywords" content="slowa klucz" />
+    <meta http-equiv="X-YA-Compatible" content="IE=edge,chrome=1" />
+
+    <link rel="stylesheet" href="wyniki.css">
+</head>
+
+<body>
+    
+    <?php 
+
+    session_start();
+
+    $slowo = $_SESSION['slowo'];
+    $popr_odp = $_SESSION['popr_odp'] ;
+    $popr_odp_2 = $_SESSION['popr_odp2'];
+    $ilosc_pytan = $_SESSION['ilosc_pytan'];
+
+    $odpowiedzi = array();
+
+    for ($i = 0; $i < $ilosc_pytan; $i++) {
+        array_push ($odpowiedzi, $_POST["odpowiedz$i"]);
+        
+    }
+
+// Zmiena stringa na małe litery \\
+    for($i=0; $i < $ilosc_pytan; $i++) {
+        $slowo[$i] = strtolower($slowo[$i]);
+        $popr_odp[$i] = strtolower($popr_odp[$i]);
+        $popr_odp_2[$i] = strtolower($popr_odp_2[$i]);
+        $odpowiedzi[$i] = strtolower($odpowiedzi[$i]);
+    }
+
+// Zliczanie uzyskanych puntków \\
+$points = 0;
+
+    for ($i = 0; $i < $ilosc_pytan; $i++) {
+        if (($odpowiedzi[$i] == $popr_odp[$i]) || ($odpowiedzi[$i] == $popr_odp_2[$i])) {
+        $points++;
+        echo $slowo[$i].": "."<span style = 'color:green'>".$odpowiedzi[$i]."</span><br />";
+        }
+        else {
+            echo $slowo[$i].": "."<span style = 'color:red'>".$odpowiedzi[$i]."</span>, ".$popr_odp[$i].", ".$popr_odp_2[$i]."<br />";
+        }
+    }
+
+    echo "Uzyskane punkty: ".$points;
+
+// Powrót na główną \\
+    echo '<form action="index.php" method="post">';
+
+    echo "<input type='submit' value='Powrót' />";
+    
+    echo "</form>";
+
+// Code by Najdz3l & MrRembes \\
+    ?>
+</body>
+</html>
+
