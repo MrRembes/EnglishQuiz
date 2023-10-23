0c669d2161fccc637092edffd1b2e6e5aba7651b
MrRembes <RembesRemb@gmail.com>
2023-10-23 21:35:03 +0200

    Podpisanie stron docelowo

--- index.php
+++ index.php
@@ -12,130 +12,43 @@
     <link rel="stylesheet" href="style.css"> <!-- CSS -->
 
 </head>
-
-<nav>
-
-
-<nav>
-
 <body>
+    <div class = "navi">
+        <h2> Ang 00.00.00: </h2>
+            <div id = ANG>
+            <form action="index.php">
+                <input type="submit" value="ANG -> PL">
+            </form>
+            <form action="index.php">
+                <input type="submit" value="PL -> ANG">
+            </form>
+            </div>
+        <h2> Ang 00.00.00: </h2>
+            <div id = ANG>
+            <form action="index.php">
+                <input type="submit" value="ANG -> PL">
+            </form>
+            <form action="index.php">
+                <input type="submit" value="PL->ANG">
+            </form>
+            </div>
+        <h2> Ang 00.00.00: </h2>
+            <div id = ANG>
+            <form action="index.php">
+                <input type="submit" value="ANG -> PL">
+            </form>
+            <form action="index.php">
+                <input type="submit" value="PL -> ANG">
+            </form>
+            </div>
+</div>
 
-    <?php
-    
-    // losowanie \\
-    session_start();
-
-    $db = new mysqli('localhost', 'root', '', 'angielski_slowka'); // Łączenie z bazą danych \\
-    $table = "ang_19_09_2023";
-
-    $q = "SELECT count(*) FROM $table"; // Tabela z której bierze dane \\
-    $result = $db->query($q);
-    $row = $result->fetch_row();
-    $count = $row[0];
-
-    $id_pytanie = array(
-            rand(1, $count) // Losowanie słówka \\
-    );
-
-    $ilosc_pytan = 15;
-
-    // Losowanie i dopisanie do arraya liczb bez powtórzeń \\
-    for($i = 0; $i < $ilosc_pytan-1; $i++) { 
-
-        $rand = rand(1, $count);
-        $break = false;
-
-        for($j = 0; $j <= $i; $j++) {
-            if($id_pytanie[$j] == $rand) {
-            $i--;
-            $break = true;
-            }
-        }
-        if(!$break) {
-            array_push ($id_pytanie, $rand);
-        }
-        
-    }
-    sort($id_pytanie); // Sprawdzenie czy są powtórzenia \\
-
-    $slowo = array();
-    $popr_odp = array();
-    $popr_odp_2 = array();
-
-    //1
-    $ids = join("','", $id_pytanie); // join(separator, array) \\
-    $q = "SELECT * FROM $table WHERE id IN ('$ids')"; // Tabela z której bierze dane \\
-    $result = $db->query($q);
-
-    while ($row = $result->fetch_array()) {
-        array_push ($slowo, $row[1]);
-        array_push ($popr_odp, $row[2]);
-        array_push ($popr_odp_2, $row[3]);
-    }
-
-    for($i=0; $i < $ilosc_pytan; $i++) {
-        $popr_odp[$i] = strtolower($popr_odp[$i]);
-        $popr_odp_2[$i] = strtolower($popr_odp_2[$i]);
-    }
-
-    ?>
-<?php 
-    //formulash \\
-    $_SESSION['slowo'] = $slowo;
-    $_SESSION['popr_odp'] = $popr_odp;
-    $_SESSION['popr_odp2'] = $popr_odp_2;
-    $_SESSION['ilosc_pytan'] = $ilosc_pytan;
-    echo "<div class = 'pytania'>";
-    echo '<form action="wyniki.php" method="post">';
-    echo "<h2>Przetłumacz na polski: </h2>";
-    
-    
-    // function tip() {
-    //     $fstchar = mb_substr($popr_odp[$i], 0, 1);
-    //     return $fstchar;
-    // }
-
-    for ($i = 0; $i < $ilosc_pytan; $i++) {
-    
-        echo $i+1;
-        echo '. ';
-
-        print_r ($slowo[$i]); 
-        echo "&nbsp";
-        
-
-        // Wyświetlanie pierwszej litery z popr_odp \\
-       // echo '<button onclick=" ">Podpowiedź</button>';
-
-        echo "<br />";
-
-
-        echo "<input type='text'"; 
-        echo "name='odpowiedz$i'"; // Numer słówka
-        echo "maxlenght = '50'";
-        echo "required autocomplete='off'/><br />"; // autocomplete='off' bez odpowiedzi
-    }
-    echo "<br /><input type='submit' value='Wyślij' />";
-    echo "</form></div></center>";
-
-// echo "<button type='button'>".$popr_odp[$i]."</button>";
-// $fstchar = mb_substr($popr_odp[$i], 0, 1);
-// echo $fstchar;
-
-
-// Wybór tabeli na początku \\
-// Wybór czy z ang-pl czy pl-ang \\
-// Zrobić CSS \\
-// Dodać przycisk podpowiedzi pokazujący pierwszą litere \\
-// W wyniki.php niech wyswietla pierwsza litere z slowo i poprawne odpowiedzi z duzej \\
-// github \\
-
-// Code by Najdz3l & MrRembes \\
-    ?>
+</body>
 
 <footer>
     <a> Powered by MrRembes and Najdz3l</a>
 </footer>
-
-</body>
+<!--                              -->
+<!-- DO POPRAWY RWD NA SUBMITACH I FOOTER DLA MAŁYCH WARTOŚCI -->
+<!--                              -->
 </html>
