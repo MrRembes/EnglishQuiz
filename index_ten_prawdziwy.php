4f9857fbcbcc5f974d5cfb7d0ab52296dfd0af35
MrRembes <RembesRemb@gmail.com>
2023-10-23 03:09:05 +0200

    Zrobienie strony tytyłowej

---
+++ index_ten_prawdziwy.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>index</title>
+    <meta name="description" content="opis" />
+    <meta name="keywords" content="slowa klucz" />
+    <meta http-equiv="X-YA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="style.css"> <!-- CSS -->
+
+</head>
+<body>
+    <div class = "navi">
+        <h2> Ang 00.00.00: </h2>
+            <div id = ANG>
+            <form action="index.php">
+                <input type="submit" value="ANG -> PL">
+            </form>
+            <form action="index.php">
+                <input type="submit" value="PL -> ANG">
+            </form>
+            </div>
+        <h2> Ang 00.00.00: </h2>
+            <div id = ANG>
+            <form action="index.php">
+                <input type="submit" value="ANG -> PL">
+            </form>
+            <form action="index.php">
+                <input type="submit" value="PL->ANG">
+            </form>
+            </div>
+        <h2> Ang 00.00.00: </h2>
+            <div id = ANG>
+            <form action="index.php">
+                <input type="submit" value="ANG -> PL">
+            </form>
+            <form action="index.php">
+                <input type="submit" value="PL -> ANG">
+            </form>
+            </div>
+</div>
+
+</body>
+
+<footer>
+    <a> Powered by MrRembes and Najdz3l</a>
+</footer>
+<!--                              -->
+<!-- DO POPRAWY RWD NA SUBMITACH I FOOTER DLA MAŁYCH WARTOŚCI -->
+<!--                              -->
+</html>
