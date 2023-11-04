19ebefefeeffdef00d811b1daf06f732ec97e088
Najdz3l <kryspinplplnagler@gmail.com>
2023-11-04 21:06:19 +0100

    nothing here

--- wyniki.php
+++ wyniki.php
@@ -6,6 +6,11 @@
     <meta name="description" content="opis" />
     <meta name="keywords" content="slowa klucz" />
     <meta http-equiv="X-YA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Czcionki Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="style.css">
 </head>
@@ -17,8 +22,8 @@
             <table>
                 <tr>
                     <th>Słowo</th>
-                    <th>Twoja Odpowiedź</Th>
-                    <th>Poprawna Odpowiedź</Th>
+                    <th>Twoja Odpowiedź</th>
+                    <th>Poprawna Odpowiedź</th>
                 </tr>
         <?php 
 
@@ -51,17 +56,17 @@
             echo "<tr>";
             if (($odpowiedzi[$i] == $popr_odp[$i]) || ($odpowiedzi[$i] == $popr_odp_2[$i])) {
             $points++;
-            echo "<td> $slowo[$i]:</td>";
-            echo "<td> <span style = 'color: #00FF00'>".$odpowiedzi[$i]."</span></td>";
-            echo "<td> $odpowiedzi[$i]</td>";
+            echo "<td> {$slowo[$i]}:</td>";
+            echo "<td> <span style = 'color: #00FF00'>{$odpowiedzi[$i]}</span></td>";
+            echo "<td> {$odpowiedzi[$i]}</td>";
             }
             else {
-                echo "<td>$slowo[$i]:</td>";
-                echo "<td><span style = 'color:red'>".$odpowiedzi[$i]."</span></td>";
-                echo "<td>$popr_odp[$i]";
+                echo "<td> {$slowo[$i]} </td>";
+                echo "<td><span style = 'color:red'> {$odpowiedzi[$i]} </span></td>";
+                echo "<td>{$popr_odp[$i]}";
                 
                 if (($popr_odp_2[$i] != NULL) && $angpl) {
-                    echo " / $popr_odp_2[$i]";
+                    echo " / {$popr_odp_2[$i]}";
                 }
                     echo "</td>";
                 }
